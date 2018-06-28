cf24cb89c5d0ee301d8bd6bb609c4dafb3f10538
kevin <kevinjobrien420@gmail.com>
2018-06-28 03:09:49 -0500

    257 App Number 35 - Part2 - Create an Expandable ListView in Android // Coding

--- app/src/main/java/ExpandableListViewAdapter.java
+++ app/src/main/java/ExpandableListViewAdapter.java
@@ -1,43 +1,56 @@
+import android.content.Context;
+import android.graphics.Color;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 public class ExpandableListViewAdapter extends BaseExpandableListAdapter{
 
 
     String[] groupNames = {"Sport", "Computer", "Food", "Car", "TV"};
     String[][] childNames = {{"Boxing", "Lick Boxing", "Judo", "Football", "Basketball"},
-            {"Desktop Computer", "Laptop Computer", "SpaceForce Computer"},
+            {"Desktop Computer", "Laptop Computer","SmartPhone Computer", "SpaceForce Computer"},
             {"Ice Cream", "Banana"}, {"Mercedes Benz"}, {"Samsung TV", "LG TV"}};
+
+    //Constructor
+    Context context;
+
+    public ExpandableListViewAdapter(Context context) {
+
+        this.context = context;
+    }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return groupNames.length;
     }
 
     @Override
     public int getChildrenCount(int i) {
-        return 0;
+        return childNames.length;
     }
 
     @Override
-    public Object getGroup(int i) {
-        return null;
+    public Object getGroup(int groupPosition) {
+        return groupNames[groupPosition];
     }
 
     @Override
-    public Object getChild(int i, int i1) {
-        return null;
+    public Object getChild(int groupPosition, int childPosition) {
+        return childNames[groupPosition][childPosition];
     }
 
     @Override
-    public long getGroupId(int i) {
-        return 0;
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
     }
 
     @Override
-    public long getChildId(int i, int i1) {
-        return 0;
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
     }
 
     @Override
@@ -46,17 +59,24 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter{
     }
 
     @Override
-    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+    public View getGroupView(int groupPosition, boolean isExpanded, View view, ViewGroup parent) {
+
+        TextView txtView = new TextView(context);
+        txtView.setText(groupNames[groupPosition]);
+        txtView.setPadding(100,0,0,0);
+        txtView.setTextColor(Color.BLUE);
+        txtView.setTextSize(40);
+        return txtView;
+
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean b, View view, ViewGroup viewGroup) {
         return null;
     }
 
     @Override
-    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-        return null;
-    }
-
-    @Override
-    public boolean isChildSelectable(int i, int i1) {
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
         return false;
     }
 }
